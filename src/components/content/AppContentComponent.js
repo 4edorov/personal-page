943f059f7ea45489ca81558ac5a1e26b988f4d47
4edorov <4edorov@gmail.com>
2017-07-03 08:32:57 +0300

    optimization with scrolling #5

--- src/components/content/AppContentComponent.js
+++ src/components/content/AppContentComponent.js
@@ -35,25 +35,44 @@ const mapDispatchToProps = (dispatch) => ({
 class AppContentComponent extends Component {
   listenEventScroll = (event) => {
     let greetingTop = event.target.all['greeting'].getBoundingClientRect().top;
-    let aboutTop = event.target.all['about-me'].getBoundingClientRect().top;
-    let portfolioTop = event.target.all['portfolio'].getBoundingClientRect().top;
-    let wayTop = event.target.all['my-way'].getBoundingClientRect().top;
-    let contactsTop = event.target.all['contacts'].getBoundingClientRect().top;
+    let greetingHeight = event.target.all['greeting'].clientHeight;
 
-    if (greetingTop <= 0) {
-      this.props.changeStateApp(STATE_APP[0]);
+    let aboutTop = event.target.all['about-me'].getBoundingClientRect().top;
+    let aboutHeight = event.target.all['about-me'].clientHeight;
+
+    let portfolioTop = event.target.all['portfolio'].getBoundingClientRect().top;
+    let portfolioHeight = event.target.all['portfolio'].clientHeight;
+
+    let wayTop = event.target.all['my-way'].getBoundingClientRect().top;
+    let wayHeight = event.target.all['my-way'].clientHeight;
+
+    let contactsTop = event.target.all['contacts'].getBoundingClientRect().top;
+    let contactsHeight = event.target.all['contacts'].clientHeight;
+
+    if (greetingTop <= 0 && greetingTop >= -greetingHeight) {
+      if (this.props.stateApp !== STATE_APP[0]) {
+        this.props.changeStateApp(STATE_APP[0]);
+      }
     }
-    if (aboutTop <= 0) {
-      this.props.changeStateApp(STATE_APP[1]);
+    if (aboutTop <= 0 && aboutTop >= -aboutHeight) {
+      if (this.props.stateApp !== STATE_APP[1]) {
+        this.props.changeStateApp(STATE_APP[1]);
+      }
     }
-    if (portfolioTop <=0) {
-      this.props.changeStateApp(STATE_APP[2]);
+    if (portfolioTop <=0 && portfolioTop >= -portfolioHeight) {
+      if (this.props.stateApp !== STATE_APP[2]) {
+        this.props.changeStateApp(STATE_APP[2]);
+      }
     }
-    if (wayTop <= 0) {
-      this.props.changeStateApp(STATE_APP[3]);
+    if (wayTop <= 0 && wayTop >= -wayHeight) {
+      if (this.props.stateApp !== STATE_APP[3]) {
+        this.props.changeStateApp(STATE_APP[3]);
+      }
     }
-    if (contactsTop <= 0) {
-      this.props.changeStateApp(STATE_APP[4]);
+    if (contactsTop <= 0 && contactsTop >= -contactsHeight) {
+      if (this.props.stateApp !== STATE_APP[4]) {
+        this.props.changeStateApp(STATE_APP[4]);
+      }
     }
   }
   componentDidMount() {
