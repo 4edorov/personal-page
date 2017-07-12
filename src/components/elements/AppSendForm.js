517bdad1836c0909d9cabd3aa96514e13fe486b2
4edorov <4edorov@gmail.com>
2017-07-12 08:56:32 +0300

    edit send form

--- src/components/elements/AppSendForm.js
+++ src/components/elements/AppSendForm.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles, createStyleSheet } from 'material-ui/styles';
 import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
 import { URL_FOR_SEND_EMAIL_FORM } from '../../config/AppConfig';
 
 
 class AppSendForm extends Component {
   state = {
     name: '',
+    email: '',
+    message: '',
   };
 
   render() {
@@ -25,6 +28,22 @@ class AppSendForm extends Component {
             onChange={event => this.setState({ name: event.target.value })}
             marginForm
           />
+          <TextField
+            id="email"
+            label="Email"
+            value={this.state.email}
+            onChange={event => this.setState({ email: event.target.value })}
+            InputProps={{ placeholder: 'your.name@email.com'}}
+            marginForm
+          />
+          <TextField
+            id="message"
+            label="Message"
+            value={this.state.message}
+            onChange={event => this.setState({ message: event.target.value })}
+            marginForm
+          />
+          <Button raised>Send</Button>
         </form>
       </div>
     )
