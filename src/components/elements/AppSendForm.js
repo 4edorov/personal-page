4d822ff02bb5f0588cae447024c2757eaf3977a6
4edorov <4edorov@gmail.com>
2017-07-14 08:56:57 +0300

    edit app send form #4

--- src/components/elements/AppSendForm.js
+++ src/components/elements/AppSendForm.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles, createStyleSheet } from 'material-ui/styles';
+import Card, { CardHeader, CardContent, CardActions } from 'material-ui/Card';
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 import { URL_FOR_SEND_EMAIL_FORM } from '../../config/AppConfig';
 
+
+const styleSheet = createStyleSheet('AppSendForm', {
+  container: {
+    width: 300,
+  },
+  inputForms: {
+    display: 'flex',
+    flexDirection: 'column',
+  }
+});
 
 class AppSendForm extends Component {
   state = {
@@ -14,40 +25,53 @@ class AppSendForm extends Component {
   };
 
   render() {
+    const classes = this.props.classes;
     return (
-      <div>
-        <form
-          id="gform"
-          method="POST"
-          action={URL_FOR_SEND_EMAIL_FORM}
-        >
-          <TextField
-            id="name"
-            label="Name"
-            value={this.state.name}
-            onChange={event => this.setState({ name: event.target.value })}
-            marginForm
-          />
-          <TextField
-            id="email"
-            label="Email"
-            value={this.state.email}
-            onChange={event => this.setState({ email: event.target.value })}
-            InputProps={{ placeholder: 'your.name@email.com'}}
-            marginForm
-          />
-          <TextField
-            id="message"
-            label="Message"
-            value={this.state.message}
-            onChange={event => this.setState({ message: event.target.value })}
-            marginForm
-          />
-          <Button raised>Send</Button>
-        </form>
+      <div className={classes.container}>
+        <Card>
+          <CardHeader title="Contact Form" />
+          <CardContent className={classes.inputForms}>
+            <TextField
+              id="name"
+              label="Name"
+              InputProps={{ placeholder: 'Enter your name'}}
+              value={this.state.name}
+              onChange={event => this.setState({ name: event.target.value})}
+              marginForm
+            />
+            <TextField
+              id="email"
+              label="Email"
+              InputProps={{ placeholder: 'your.name@name.com'}}
+              value={this.state.email}
+              onChange={event => this.setState({ email: event.target.value})}
+              marginForm
+            />
+            <TextField
+              id="multiline-static"
+              multiline
+              rows="4"
+              label="Message"
+              InputProps={{ placeholder: 'What\'s on your mind...'}}
+              value={this.state.message}
+              onChange={event => this.setState({ message: event.target.value})}
+              marginForm
+            />
+          </CardContent>
+          <CardActions>
+            <Button raised>Send</Button>
+          </CardActions>
+
+          <form
+            id="gform"
+            method="POST"
+            action={URL_FOR_SEND_EMAIL_FORM}
+          >
+          </form>
+        </Card>
       </div>
     )
   }
 };
 
-export default AppSendForm;
+export default withStyles(styleSheet)(AppSendForm);
