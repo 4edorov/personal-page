ae67e8c3cf2f103d483fb75ec1da177e14095e4b
4edorov <4edorov@gmail.com>
2017-07-16 14:23:32 +0300

    basic version of send form #4

--- src/components/elements/AppSendForm.js
+++ src/components/elements/AppSendForm.js
@@ -7,7 +7,10 @@ import Card, { CardHeader, CardContent, CardActions } from 'material-ui/Card';
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
+import Icon from 'material-ui/Icon';
 import Close from 'material-ui-icons/Close';
+import Cancel from 'material-ui-icons/Cancel';
+import Check from 'material-ui-icons/Check';
 import { URL_FOR_SEND_EMAIL_FORM } from '../../config/AppConfig';
 
 
@@ -37,55 +40,41 @@ const mapDispatchToProps = (dispatch) => ({
 
 class AppSendForm extends Component {
   state = {
-    name: '',
-    email: '',
-    message: '',
+    form: {
+      name: '',
+      email: '',
+      message: '',
+    },
+    checkEmail: false,
   };
   handleDrawerClose = () => {
     this.props.toggleSendDrawer(false);
   };
 
-  getFormData = (elements) => {
-    return elements;
+  validEmail = (email) => {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+  }
+
+  getFormData = (messageFields) => {
+    let validateMessageFields = {};
+    Object.keys(messageFields).forEach(field => {
+      if (messageFields[field].length > 0) {
+        validateMessageFields[field] = messageFields[field];
+      }
+    });
+    return validateMessageFields;
   };
 
   handleFormSubmit = () => {
-    console.log('handleSubmit');
-    const data = this.getFormData(this.state);
-    console.log('data', JSON.stringify(data));
+    const data = this.getFormData(this.state.form);
     let url = URL_FOR_SEND_EMAIL_FORM;
     let xhr = new XMLHttpRequest();
     xhr.open('POST', url);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.send(JSON.stringify(data));
+    this.handleDrawerClose();
   };
-
-  // function handleFormSubmit(event) {  // handles form submit withtout any jquery
-  //   event.preventDefault();           // we are submitting via xhr below
-  //   var data = getFormData();         // get the values submitted in the form
-  //   if( !validEmail(data.email) ) {   // if email is not valid show error
-  //     document.getElementById('email-invalid').style.display = 'block';
-  //     return false;
-  //   } else {
-  //     var url = event.target.action;  //
-  //     var xhr = new XMLHttpRequest();
-  //     xhr.open('POST', url);
-  //     // xhr.withCredentials = true;
-  //     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  //     xhr.onreadystatechange = function() {
-  //         console.log( xhr.status, xhr.statusText )
-  //         console.log(xhr.responseText);
-  //         document.getElementById('gform').style.display = 'none'; // hide form
-  //         document.getElementById('thankyou_message').style.display = 'block';
-  //         return;
-  //     };
-  //     // url encode form data for sending as post data
-  //     var encoded = Object.keys(data).map(function(k) {
-  //         return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
-  //     }).join('&')
-  //     xhr.send(encoded);
-  //   }
-  // }
 
   render() {
     const classes = this.props.classes;
@@ -106,26 +95,38 @@ class AppSendForm extends Component {
               id="name"
               label="Name"
               InputProps={{ placeholder: 'Enter your name'}}
-              value={this.state.name}
-              onChange={event => this.setState({ name: event.target.value})}
+              value={this.state.form.name}
+              onChange={event => this.setState({ form: {...this.state.form, name: event.target.value}})}
               marginForm
             />
-            <TextField
-              id="email"
-              label="Email"
-              InputProps={{ placeholder: 'your.name@name.com'}}
-              value={this.state.email}
-              onChange={event => this.setState({ email: event.target.value})}
-              marginForm
-            />
+            <div>
+              <TextField
+                id="email"
+                label="Email"
+                InputProps={{ placeholder: 'your.name@name.com'}}
+                value={this.state.form.email}
+                onChange={event => {
+                  this.setState({ form: {...this.state.form, email: event.target.value}});
+                  let checkEmail = this.validEmail(this.state.form.email);
+                  if (checkEmail) {
+                    this.setState({ checkEmail: true });
+                  } else {
+                    this.setState({ checkEmail: false });
+                  }
+                }}
+                marginForm
+              />
+              {this.state.checkEmail ? <Icon><Check /></Icon> : <Icon><Cancel /></Icon>}
+            </div>
+
             <TextField
               id="multiline-static"
               multiline
               rows="4"
               label="Message"
               InputProps={{ placeholder: 'What\'s on your mind...'}}
-              value={this.state.message}
-              onChange={event => this.setState({ message: event.target.value})}
+              value={this.state.form.message}
+              onChange={event => this.setState({ form: {...this.state.form, message: event.target.value}})}
               marginForm
             />
           </CardContent>
