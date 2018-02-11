a243fcd8415169bd7c9ba4c642e74a05d0555f52
4edorov <4edorov@gmail.com>
2018-02-11 22:08:34 +0300

    edit greetings page #52

--- src/components/content/ContentGreetingComponent.js
+++ src/components/content/ContentGreetingComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
+import bgGreeting from '../../assets/static/images/bg_greeting.jpg';
 
 
 const styleSheet = theme => ({
@@ -8,22 +9,44 @@ const styleSheet = theme => ({
     fontSize: '1.5em',
     textAlign: 'center',
     lineHeight: '1.5',
+    color: theme.palette.secondary['500'],
+  },
+  baseLayout: {
+    width: '100%',
+  },
+  basePage: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    backgroundSize: '100% 100%',
+    backgroundImage: 'url(' + bgGreeting + ')',
+    backgroundAttachment: 'fixed',
+    backgroundPosition: 'center center',
+  },
+  contentHeader: {
+    color: theme.palette.secondary['500'],
   },
 });
 
 const ContentGreetingComponent = props => {
   const classes = props.classes;
   return (
-    <div id="greeting">
-      <div className="content">
-        <div>
-          <h1>Hello!</h1>
-        </div>
-        <div>
-          <p className={classes.contentPage}>My Name Is Alexander Fedorov</p>
-          <p className={classes.contentPage}>I&#39;m A Junior Web Developer</p>
-          <p className={classes.contentPage}>This Site About Me</p>
-          <p className={classes.contentPage}>& My Favorite Thing</p>
+    <div id='greeting'>
+      <div className='content'>
+        <div className={classes.baseLayout}>
+          <div className={classes.basePage}>
+            <div>
+              <h1 className={classes.contentHeader}>Hello!</h1>
+            </div>
+            <div>
+              <p className={classes.contentPage}>My Name Is Alexander Fedorov</p>
+              <p className={classes.contentPage}>I&#39;m A Junior Web Developer</p>
+              <p className={classes.contentPage}>This Site About Me</p>
+              <p className={classes.contentPage}>& My Favorite Thing</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
