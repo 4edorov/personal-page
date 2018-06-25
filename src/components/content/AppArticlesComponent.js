42784099434b5ddf4694242d3829a82ef063d103
4edorov <4edorov@gmail.com>
2018-06-25 10:03:34 +0300

    added react-markdown #78

--- src/components/content/AppArticlesComponent.js
+++ src/components/content/AppArticlesComponent.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactMarkdown from 'react-markdown';
 import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 import { changeStateApp } from '../../actions';
+import { article01 } from './articlesData/article01';
 
 const styleSheet = theme => ({
   articlesContent: {
@@ -22,6 +24,12 @@ const styleSheet = theme => ({
     margin: 50,
     height: '100vh',
   },
+  blockquote: {
+    color: '#666',
+    margin: 0,
+    paddingLeft: '3em',
+    borderLeft: '0.5em #eee solid',
+}
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -29,6 +37,17 @@ const mapDispatchToProps = dispatch => ({
     dispatch(changeStateApp(shouldState))
   }
 });
+
+// const DefaultCodeRenderer = ReactMarkdown.renderers.code;
+
+// const CodeRenderer = props => {
+//   return <div className="bg-black-10 br2 pa2"><DefaultCodeRenderer {...props} /></div>
+// }
+
+const BlockQuoteRenderer = props => {
+  return <blockquote>{props.children}</blockquote>
+}
+
 
 class AppArticlesComponent extends React.Component {
   componentDidMount () {
@@ -41,8 +60,9 @@ class AppArticlesComponent extends React.Component {
       <div className={classes.articlesContent}>
         <div className={classes.root}>
           <h2>Sorry, this part is under construction</h2>
+          <ReactMarkdown source={article01} renderers={{blockquote: BlockQuoteRenderer}} />
         </div>
-      </div>
+      </div>  
     );
   }
 }
